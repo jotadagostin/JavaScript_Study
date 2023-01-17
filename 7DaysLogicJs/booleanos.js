ee93e7520a858514911e84e20c3a3eb3fa618cb0
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-01-17 10:53:39 -0700

    booleans

---
+++ 7DaysLogicJs/booleanos.js
@@ -0,0 +1,26 @@
+// BOOLEANOS:
+
+let numberOne = 1;
+let StringOne = '1';
+let NumberThirty = 30;
+let StringThirty = '30';
+let numberTen = 10;
+let stringTen = '10';
+
+if(numberOne == StringOne) {
+    console.log('Variables numberOne and stringOne have the same value but different types')
+} else {
+    console.log('Variables numberOne and stringOne do not have the same value')
+}
+
+if(NumberThirty === StringThirty) {
+    console.log('Variables numberThirty and stringThirty are the same valye and type')
+} else {
+    console.log('Variables numberThirty and stringThirty are not the same type')
+}
+
+if (numberTen == stringTen) {
+    console.log('Variables numberTen and stringTen have the same value, but different types')
+} else {
+    console.log('variables numberTen and stringTen do not have the same value')
+}
