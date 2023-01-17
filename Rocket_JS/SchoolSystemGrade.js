30bb2c780efa419d0c0940535f3e44d3260cf6e9
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-01-16 21:59:34 -0700

    Create SchoolSystemGrade.js

---
+++ Rocket_JS/SchoolSystemGrade.js
@@ -0,0 +1,29 @@
+// SCHOOL SYSTEM OF GRADES: NOTES (90,80...) TO LETTERS(A, B, C...):
+
+function getScore(score) {
+    let scoreA = score >= 90 && score <= 100;
+    let scoreB = score >= 80 && score <= 89;
+    let scoreC = score >= 70 && score <= 79;
+    let scoreD = score >= 60 && score <= 69;
+    let scoreF = score < 60 && score >= 0;
+
+    let scoreFinal;
+
+    if (scoreA) {
+        scoreFinal = "A"
+    } else if (scoreB) {
+        scoreFinal = "B"
+    } else if (scoreC) {
+        scoreFinal = "C"
+    } else if (scoreD) {
+        scoreFinal = "D"
+    } else if (scoreF) {
+        scoreFinal = "F"
+    } else {
+        scoreFinal = "Invalid grade"
+    }
+
+    return scoreFinal;
+}
+
+console.log(getScore(90));
