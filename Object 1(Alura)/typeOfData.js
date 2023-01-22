4f343c9427b0a2651fbfebb7c6b772060d4abb7f
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-01-22 13:17:50 -0700

    Create typeOfData.js

---
+++ Object 1(Alura)/typeOfData.js
@@ -0,0 +1,11 @@
+// TYPE OS DATA AND VALUE:
+
+const client = {
+    name: 'joao',
+    age: 32,
+    cpf: '12345679854',
+    email: 'joao@dominio.com',
+    phone: ['95652654', '98745182']
+}
+
+console.log(client);
