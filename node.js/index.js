7d1882921e14846c66562d883eb49498a226969a
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-01-25 10:37:49 -0700

    node.js

---
+++ node.js/index.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+            if (erro) {
+                trataErro(erro);
+            }
+        console.log(chalk.green(texto))
+    })
+}
+
+pegaArquivo('./arquivos/');
+
+
